3524af92b899ba1606e313403f6bbb7bb42e1d53
rubelrana123 <rubelranatpi707458@gmail.com>
2026-02-03 23:56:09 +0600

    update host modules

--- src/app/modules/host/host.routes.ts
+++ src/app/modules/host/host.routes.ts
@@ -7,12 +7,12 @@ import { HostValidation } from './host.validation';
 
 const router = express.Router();
 
- 
+ // Create a new host
 router.get('/', HostController.getAllFromDB);
 
- 
+ // Get host by ID
 router.get('/:id', HostController.getByIdFromDB);
-
+// update a new host
 router.patch(
     '/:id',
     auth(  UserRole.ADMIN, UserRole.HOST, UserRole.PARTICIPATOR),
@@ -20,14 +20,14 @@ router.patch(
     HostController.updateIntoDB
 );
 
- 
+ // delete a host
 router.delete(
     '/:id',
     auth(UserRole.ADMIN),
     HostController.deleteFromDB
 );
 
- 
+ // soft delete a host
 router.delete(
     '/soft/:id',
     auth(UserRole.ADMIN),
